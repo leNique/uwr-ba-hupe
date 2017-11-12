2dbdef14c39b8d1d64987ae09126f8610ea10d6d
leNique <lot_nik@gmx.de>
2017-11-12 19:15:58 +0100

    Langes Hupen verkürzt
    Bei der Bundesliga runde wurde 2 mal ein anhupen nicht erkannt.
    Deshalb habe ich das benötigte Signal von 400 auf 380 Millisekunden
    verkürzt

--- Code/src/LangesSignal.ino
+++ Code/src/LangesSignal.ino
@@ -2,25 +2,25 @@ void LangesSignal (int i, bool val)
 {
         if ((Stop != 0 && Stop + 8000 < millis()) || DurchlaufendeSpielzeit)    //nur ausführen wenn Zeit schon 8 Sec steht oder durchlaufende Zeit
         {
-                if (val == 1 && LangesHupenStatus[i] == 0)
+                if (val == 1 && LangesHupenStatus[i] == 0)    // Es wird gehupt und die Zeitmessung startet für langeshupen
                 {
                         TimerLangesHupen[i] = millis();
                         LangesHupenStatus[i] = 1;
                         LangesHupenFehler[i] = 0;
                 }
 
-                if (val == 0 && LangesHupenStatus[i] == 1)
+                if (val == 0 && LangesHupenStatus[i] == 1)   // Der Taster ist nicht gedrückt während Hupen (Fehler)
                 {
                         LangesHupenFehler[i]++;
                 }
 
-                if (LangesHupenStatus[i] == 1 && TimerLangesHupen[i] + 700 < millis())
+                if (LangesHupenStatus[i] == 1 && TimerLangesHupen[i] + 700 < millis())    // es wurde kein langes Hupsignal erkannt - die Messung wird verworfen
                 {
                         TimerLangesHupen[i] = 0;
                         LangesHupenStatus[i] = 0;
                 }
 
-                if (val == 1 && LangesHupenStatus[i] == 1 && LangesHupenFehler[i] < 250 && TimerLangesHupen[i] + 400 < millis()) // 400 Millisekunden muss für langes Hupen mindestens gehupt werden
+                if (val == 1 && LangesHupenStatus[i] == 1 && LangesHupenFehler[i] < 250 && TimerLangesHupen[i] + 380 < millis()) // 380 Millisekunden muss für langes Hupen mindestens gehupt werden
                 {
                         TimerLangesHupen[i] = 0;     //LangesHupenerkannt    Uhr läuft weiter
                         LangesHupenStatus[i] = 0;
