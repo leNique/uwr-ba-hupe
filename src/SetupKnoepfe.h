883629e22c605ffbe77806c45693504bb68ae49f
leNique <lot_nik@gmx.de>
2022-12-19 10:08:48 +0100

    Create SetupKnoepfe.h

---
+++ src/SetupKnoepfe.h
@@ -0,0 +1,6 @@
+#ifndef gloabals_H
+#define gloabals_H
+#include "globals.h"
+#endif 
+
+void SetupKnoepfe(bool isButtonResetPressed, bool isButtonSetupPressed, bool isButtonPlusPressed, bool isButtonMinusPressed);
