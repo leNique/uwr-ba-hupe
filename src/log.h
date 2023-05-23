4f4cea4be18d40d49bf5659e2b7b3ccf723c8e95
leNique <lot_nik@gmx.de>
2023-05-23 11:14:50 +0200

    Update log.h

--- src/log.h
+++ src/log.h
@@ -3,3 +3,7 @@
 #include "globals.h"
 #endif 
 
+
+void SD_WRITE_LOG();
+void readDS1307time(byte *second, byte *minute, byte *hour, byte *dayOfWeek, byte *dayOfMonth, byte *month, byte *year);
+byte bcdToDec(byte val);
