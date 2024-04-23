a7988efc3a8735199812a6f0273106d339c45e99
leNique <lot_nik@gmx.de>
2024-04-23 14:40:06 +0200

    Update log.h

--- src/log.h
+++ src/log.h
@@ -5,6 +5,11 @@
 
 // Zum Logen wird dieses Modul verwendet - Wemos D1 Data Logger Mini Shield+RTC DS1307 Clock For -
 
+// change this to match your SD shield or module;
+// WeMos Micro SD Shield V1.0.0: D8
+// LOLIN Micro SD Shield V1.2.0: D4 (Default)
+const int chipSelect = D4;
+
 
 void SD_WRITE_LOG();
 void readDS1307time(byte *second, byte *minute, byte *hour, byte *dayOfWeek, byte *dayOfMonth, byte *month, byte *year);
