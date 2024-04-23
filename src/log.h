3c1850efc47c85d1849d6361085d9d67ef25e126
leNique <lot_nik@gmx.de>
2024-04-23 14:36:57 +0200

    Update log.h

--- src/log.h
+++ src/log.h
@@ -3,6 +3,8 @@
 #include "globals.h"
 #endif 
 
+// Zum Logen wird dieses Modul verwendet - Wemos D1 Data Logger Mini Shield+RTC DS1307 Clock For -
+
 
 void SD_WRITE_LOG();
 void readDS1307time(byte *second, byte *minute, byte *hour, byte *dayOfWeek, byte *dayOfMonth, byte *month, byte *year);
